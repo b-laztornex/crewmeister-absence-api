d9dee5a906c4c3c34dea25eb7bcf9e6fbed2259d
Marco Casanova <m.casanova.dev@gmail.com>
2025-03-01 15:33:09 +0100

    add env variables

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: "postgres",
-  user: "postgres",
-  password: "",
-  database: "crewmeister_db",
-  port: 5432,
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
 });
 
 module.exports = {
